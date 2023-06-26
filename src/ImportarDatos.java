a719e342682495ec15e41882034a260cd0981632
Diego <dahh12@gmail.com>
2023-06-26 17:18:04 -0500

    INICIO

---
+++ src/ImportarDatos.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+// se importan las librerias para ser unadas dentro del programa o la clase.
+
+
+public class ImportarDatos {
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
